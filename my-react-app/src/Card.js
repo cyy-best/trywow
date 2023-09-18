6c678255b925029d8b754af5c027f0aca2f899d1
LULU <78440615+cyy-best@users.noreply.github.com>
2023-09-18 19:24:34 -0230

    carousel disposal

--- my-react-app/src/Card.js
+++ my-react-app/src/Card.js
@@ -5,9 +5,9 @@ function Card() {
         <div className="card">
             <img src="https://via.placeholder.com/150" className="card-img-top" alt="Placeholder" />
             <div className="card-body">
-                <h5 className="card-title">Card Title</h5>
+                <h6 className="card-title">Card Title</h6>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Learn More</a>
+                <a href="#" className="btn">Learn More</a>
             </div>
         </div>
     );
