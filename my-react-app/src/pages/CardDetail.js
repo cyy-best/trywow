a1e7bb49de2f49bdeb9f4e8a8d530be8c24a40eb
lulucy <ycao19@mun.ca>
2023-11-25 17:02:29 -0330

    renderlist

--- my-react-app/src/pages/CardDetail.js
+++ my-react-app/src/pages/CardDetail.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import { Container, Row, Col} from 'react-bootstrap';
-import { FaStar} from 'react-icons/fa'; // 导入一些图标
+import { Container, Row, Col } from 'react-bootstrap';
 import Navbar from '../NavigationBar';
 import Author from '../author';
 import Comment from '../comment';
@@ -8,6 +6,29 @@ import '../css/index.css';
 import '../css/c-detail.css';
 
 function CardDetail() {
+    const title = 'detail-title';
+    const likes = 50;
+    const states = {
+        comments: [
+            { id: 1, name: 'jack', content: 'first comment' },
+            { id: 2, name: 'tom', content: 'second comment' },
+            { id: 3, name: 'jerry', content: 'third comment' }
+
+        ]
+    }
+
+    const renderList = () => {
+        return states.comments.length === 0 ?
+        (<div className='no-comments'>No comments</div>)
+        : (<ul className="comment-list">
+            {states.comments.map(items => (
+                <li key={items.id}>
+                    <h6 >{items.name}</h6>
+                    <p>{items.content}</p>
+                </li>
+            ))}
+        </ul>)
+    }
     return (
         <div className='card-detail'>
             <Navbar />
@@ -19,7 +40,7 @@ function CardDetail() {
                     {/* 中间图文内容 */}
                     <Col md={9} >
                         <div className='main-section'>
-                            <h3 className='h3'>Main Content</h3>
+                            <h3 className='h3'>{title}</h3>
                             <div className='img-container'>
                                 <div className='card-image image1'></div>
                                 <p>
@@ -41,6 +62,7 @@ function CardDetail() {
                                     vestibulum nunc fermentum. Vivamus at dui et metus ullamcorper
                                     auctor. Sed vel purus ut ex consectetur dignissim.
                                 </p>
+                                <p> liked {likes} times</p>
                             </div>
                             <Row>
                                 <Col>
@@ -54,17 +76,16 @@ function CardDetail() {
                     {/* 右侧评论和评分 */}
                     <Col md={3}>
                         {/* 右侧评论和评分 */}
-                        <div className='side-section d-flex flex-column justify-content-between'>                          
-                                <Author />
-                                <div className="comment-list">
-                                    <p>User 1: This is great!</p>
-                                    <div className="icons">
-                                        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
-                                    </div>
-                                {/* 添加更多评论 */}
-                            </div>
+                        <div className='side-section d-flex flex-column justify-content-between'>
+                            <Author />
+                            {/* 条件渲染 三元表达式*/}
+                            {
+                                renderList()
+                            }
+
                             <Comment />
                         </div>
+
                     </Col>
                 </Row>
             </Container>
